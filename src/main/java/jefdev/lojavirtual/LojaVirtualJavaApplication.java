7e341dd10e662cb67d2c48a54c57c360bc2d14b2
Jeferson Sales <jefersonsales1@gmail.com>
2024-02-11 22:29:56 -0300

    primeiro commit

---
+++ src/main/java/jefdev/lojavirtual/LojaVirtualJavaApplication.java
@@ -0,0 +1,13 @@
+package jefdev.lojavirtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaVirtualJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaVirtualJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jefdev/lojavirtual/LojaVirtualJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package jefdev.lojavirtual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaVirtualJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
